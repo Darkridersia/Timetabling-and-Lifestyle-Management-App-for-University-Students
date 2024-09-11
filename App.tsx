a5fbaf3ada2e4fcdfdcbfc0e581b4fd21e4001b8
Sia Li Ze <riderdark795@gmail.com>
2024-09-11 22:33:37 +0800

    Re-roll work?

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import LoginScreen from './screens/LoginScreen'; // Import LoginScreen
 import SignUpScreen from './screens/SignUpScreen';
 import ResultScreen from './screens/ResultScreen';
 import ChatScreen from './screens/ChatScreen';
+// Hello from the other side
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
